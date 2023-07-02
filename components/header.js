56e407ee8291fccf32963779e18e4d2abfc152a6
fengde <jun1st.feng@gmail.com>
2023-07-02 20:29:12 +0800

    fix eslint warning

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header>
             <h1 style={{marginTop: 0}}>
-                <Link href="/">fengd's personal zone</Link>
+                <Link href="/">fengd&apos;s personal zone</Link>
             </h1>
         </header>
     )
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({allPostsData}) {
   return (
     <>
       <Head>
-        <title>fengd's personal zone</title>
+        <title>fengd&apos;s personal zone</title>
         <meta name="description" content="fengde's personal blog" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
