dceb28a13b22007141cddca067a86ff734ff8482
fengde <jun1st.feng@gmail.com>
2023-09-02 14:19:36 +0800

    add viewport

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -12,6 +12,7 @@ export default function Post({ postData, id }) {
       <>
         <Head>
           <title>{postData.title}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
           <link rel="canonical" href={"https://blog.fengqijun.me/posts/"  + id} />
         </Head>
         
